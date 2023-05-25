bb92454738df06c6f0515dcac35c7223dc5e7b28
kz-gongon1006 <134474043+kz-gongon1006@users.noreply.github.com>
2023-05-25 12:37:08 +0900

    説明文の追加

---
+++ Assets/gonda/StartButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class StartButton : MonoBehaviour
+{
+    [SerializeField] Button _StartButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _StartButton.onClick.AddListener(() => SceneManager.LoadScene(""));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
