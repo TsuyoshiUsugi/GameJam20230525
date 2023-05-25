7c2feba4e58625475384880faf38d203aea69d1f
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-05-25 16:27:02 +0900

    敵のプレハブを追加、ボスの動きを実装しました

--- Assets/GotouTamaki/Boss.cs
+++ Assets/GotouTamaki/Boss.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Boss : Enemy
 {
-    Vector3 _dir = Vector3.right;
+    Vector3 _hDir = Vector3.right;
+    Vector3 _vDir = Vector3.up;
+    float _currentTime = 0;
+
+    [SerializeField]
+    float _decreaseSpeedNum = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,27 +18,53 @@ public class Boss : Enemy
     }
 
     // Update is called once per frame
+    private void Update()
+    {
+        _currentTime += Time.deltaTime;
+        //ChangeHoriSpeed();
+    }
+
     protected override void FixedUpdate()
     {
         InsideCamera();
 
         if (_isInsideCamera)
         {
-            Move();
+            HoriMove();
+            VertMove();
         }
     }
 
-    void Move()
+    void HoriMove()
     {
-        transform.position += _dir * _horiSpeed;
+        transform.position += _hDir * _horiSpeed;
 
         if(transform.position.x > Camera.main.ViewportToWorldPoint(Vector2.one).x) 
         {
-            _dir *= -1;
+            _hDir *= -1;
         }
         else if(transform.position.x < Camera.main.ViewportToWorldPoint(Vector2.zero).x)
         {
-            _dir *= -1;
+            _hDir *= -1;
         }
+    }
+
+    void VertMove()
+    {
+        transform.position += _vDir * _vertSpeed;
+
+        if (transform.position.y > Camera.main.ViewportToWorldPoint(Vector2.one).y)
+        {
+            _vDir *= -1;
+        }
+        else if (transform.position.y < Camera.main.ViewportToWorldPoint(Vector2.zero).y)
+        {
+            _vDir *= -1;
+        }
+    }
+
+    void ChangeHoriSpeed()
+    {
+        _horiSpeed -= _decreaseSpeedNum * _currentTime;
     }
 }
